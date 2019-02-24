467247ec9f191db291727a7826c0af1419a0cee1
Kevin McNerney <kmcnerney@salesforce.com>
2019-02-24 15:18:33 -0500

    fixed navbar

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -8,21 +8,19 @@ import PropTypes from 'prop-types'
 
 import Navbar from 'react-bootstrap/Navbar'
 import Nav from 'react-bootstrap/Nav'
-import NavItem from 'react-bootstrap/NavItem'
+
+// Constants
 import Constants from '../constants'
 
 export default class NavBar extends React.Component {
 	render () {
 		return (
-			<Navbar>
+			<Navbar bg="dark" variant="dark">
 				<Navbar.Brand href="#">{Constants.PROJECT_NAME}</Navbar.Brand>
-				<Navbar.Toggle />
-				<Navbar.Collapse>
-					<Nav>
-						<NavItem onClick={() => this.togglePage(Constants.LOG_IN_PAGE)}>Log In</NavItem>
-						<NavItem onClick={() => this.togglePage(Constants.SIGN_UP_PAGE)}>Sign Up</NavItem>
-					</Nav>
-				</Navbar.Collapse>
+				<Nav className="mr-auto">
+					<Nav.Link onClick={() => this.togglePage(Constants.LOG_IN_PAGE)}>Log In</Nav.Link>
+					<Nav.Link onClick={() => this.togglePage(Constants.SIGN_UP_PAGE)}>Sign Up</Nav.Link>
+				</Nav>
 			</Navbar>
 		)
 	}
